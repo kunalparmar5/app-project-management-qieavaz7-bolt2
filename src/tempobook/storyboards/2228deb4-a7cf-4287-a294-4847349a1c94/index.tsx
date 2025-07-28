8b4a255b935ddbad4a7532a9e87ed0e893439ba2
Name <kunalparmarrock@gmail.com>
2025-07-28 13:03:36 +0000

    tempo labs

---
+++ src/tempobook/storyboards/2228deb4-a7cf-4287-a294-4847349a1c94/index.tsx
@@ -0,0 +1,9 @@
+import DatabaseViewer from "@/components/DatabaseViewer";
+
+export default function DatabaseViewerStoryboard() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <DatabaseViewer />
+    </div>
+  );
+}
